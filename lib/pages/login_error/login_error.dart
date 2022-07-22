f46db3011cd3fbd799965f2d908622daf3baffeb
q <xql279671304@163.com>
2022-07-23 02:23:02 +0800

    add drawer

--- lib/pages/login_error/login_error.dart
+++ lib/pages/login_error/login_error.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:free_music/common/utils/utils.dart';
 import 'package:free_music/common/widgets/widgets.dart';
@@ -11,10 +12,33 @@ class LoginErrorPage extends StatefulWidget {
   State<LoginErrorPage> createState() => _LoginErrorPageState();
 }
 
+Widget _buildListItem(
+  BuildContext context,
+  String title,
+  String route,
+) {
+  return ListTile(
+    title: Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: AppColors.primaryBackground,
+        fontSize: duSetFontSize(16),
+      ),
+    ),
+    onTap: () {
+      Navigator.pushNamed(context, route);
+    },
+  );
+}
+
 class _LoginErrorPageState extends State<LoginErrorPage> {
+  final GlobalKey<ScaffoldState> _key = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _key,
       appBar: AppBar(
         title: const Text(
           'Playband',
@@ -40,17 +64,115 @@ class _LoginErrorPageState extends State<LoginErrorPage> {
         automaticallyImplyLeading: false,
         actions: <Widget>[
           IconButton(
-            onPressed: () => {},
+            onPressed: () => _key.currentState!.openEndDrawer(),
             icon: const Icon(Icons.menu),
             color: AppColors.primaryElement,
             tooltip: 'menu',
-            padding: EdgeInsets.only(
-              right: duSetWidth(32),
-            ),
+            padding: EdgeInsets.only(right: duSetWidth(32)),
           )
         ],
       ),
       backgroundColor: AppColors.primaryBackground,
+      endDrawer: Drawer(
+        backgroundColor: AppColors.secondElement,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              height: duSetHeight(314),
+              padding: EdgeInsets.symmetric(
+                horizontal: duSetWidth(30),
+              ),
+              decoration: const BoxDecoration(
+                color: AppColors.primaryElement,
+              ),
+              child: DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                        height: duSetHeight(70),
+                        width: duSetWidth(252),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: const [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Icon(
+                              Icons.menu,
+                              color: AppColors.primaryBackground,
+                            ),
+                          ],
+                        )),
+                    const SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Image(
+                        image: AssetImage('assets/images/profile.png'),
+                      ),
+                    ),
+                    Text(
+                      'Adam Collester',
+                      style: TextStyle(
+                        color: AppColors.primaryBackground,
+                        fontSize: duSetFontSize(16),
+                        height: 2,
+                      ),
+                    ),
+                    Text(
+                      'Online',
+                      style: TextStyle(
+                        color: AppColors.secondText,
+                        fontSize: duSetFontSize(12),
+                        height: 2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: duSetHeight(60),
+                bottom: duSetHeight(80),
+              ),
+              child: Column(
+                children: <Widget>[
+                  _buildListItem(context, 'Profile', '/sign-in'),
+                  _buildListItem(context, 'Albums', '/sign-in'),
+                  _buildListItem(context, 'Tracks', '/sign-in'),
+                  _buildListItem(context, 'Settings', '/sign-in'),
+                ],
+              ),
+            ),
+            GestureDetector(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Image(
+                    width: 16,
+                    height: 20,
+                    image: AssetImage('assets/images/log_out.png'),
+                    alignment: Alignment.bottomLeft,
+                  ),
+                  Text(
+                    'Logout',
+                    style: TextStyle(
+                      color: AppColors.primaryBackground,
+                      fontSize: duSetFontSize(14),
+                    ),
+                  )
+                ],
+              ),
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              },
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: <Widget>[
           const Spacer(flex: 3),
